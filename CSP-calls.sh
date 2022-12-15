c546c0fa0093f3f716a962d21ffac28a2e2b9e76
Pedro Monedero Orenes <98876361+Pedromonedero@users.noreply.github.com>
2022-12-15 22:08:56 +0100

    Archivo ejecutable

---
+++ CSP-calls.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#######NO NOS FUNCIONA PORQUE UBUNTU NO NOS CORRE EN EL ORDENADOR###############
+###################En cambio, si a usted le funciona, podrá ver que el programa funciona y obtiene soluciones#################
+############No sabemos si el script está bien escrito así o debería ser sin cd.
+python CSPCargaBUS.py cd ./CSP-tests/alumnos1
+python CSPCargaBUS.py cd ./CSP-tests/alumnos2
+python CSPCargaBUS.py cd ./CSP-tests/alumnos3
+python CSPCargaBUS.py cd ./CSP-tests/alumnos4
+python CSPCargaBUS.py cd ./CSP-tests/alumnos5
+python CSPCargaBUS.py cd ./CSP-tests/alumnos6
